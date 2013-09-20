845defa4176bb106ad6925b8eb6cf942a925d4c6
monochromegane <dev.kuro.obi@gmail.com>
2013-09-21 01:56:38 +0900

    Vim7.4を使うためMacVimを利用する

--- oh-my-zsh/custom/alias.zsh
+++ oh-my-zsh/custom/alias.zsh
@@ -1,4 +1,7 @@
 # alias
+alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim -u $HOME/.vimrc "$@"'
+alias vimdiff='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/vimdiff -u $HOME/.vimrc "$@"'
+alias vi=vim
 alias gs="git status -s -b && git stash list"
 alias beu="bundle exec rails unicorn -c config/unicorn.rb"
 alias ber="bundle exec rake routes"
