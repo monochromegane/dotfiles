335e187139382945701b64fe412f2c2f197853a7
monochromegane <dev.kuro.obi@gmail.com>
2014-05-03 13:20:16 +0900

    bundlerプラグインでよく利用していたbeエイリアスだけ個別に定義

--- oh-my-zsh/custom/alias.zsh
+++ oh-my-zsh/custom/alias.zsh
@@ -3,6 +3,7 @@ alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim -u $
 alias vimdiff='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/vimdiff -u $HOME/.vimrc "$@"'
 alias vi=vim
 alias gs="git status -s -b && git stash list"
+alias be='bundle exec'
 alias beu="bundle exec rails unicorn -c config/unicorn.rb"
 alias ber="bundle exec rake routes"
  
