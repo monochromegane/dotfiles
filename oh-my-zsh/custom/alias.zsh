c213ebce0047059c3ec2716f7c185f3f9d50cc59
monochromegane <dev.kuro.obi@gmail.com>
2015-09-19 12:50:25 +0900

    MacVimが入っている場合にvimにエイリアスをはる

--- oh-my-zsh/custom/alias.zsh
+++ oh-my-zsh/custom/alias.zsh
@@ -1,11 +1,12 @@
 # alias
-alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim -u $HOME/.vimrc "$@"'
-alias vimdiff='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/vimdiff -u $HOME/.vimrc "$@"'
+if [ -e /Applications/MacVim.app ]; then
+  alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim -u $HOME/.vimrc "$@"'
+  alias vimdiff='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/vimdiff -u $HOME/.vimrc "$@"'
+fi
 alias vi=vim
 alias gs="git status -s -b && git stash list"
 alias be='bundle exec'
 alias bi='bundle install'
-alias beu="bundle exec rails unicorn -c config/unicorn.rb"
 alias rakt="rake -T"
 alias rakr="rake routes"
 alias rake="noglob rake"
@@ -15,7 +16,6 @@ alias f="fg"
 alias bundle='nocorrect bundle'
 
 alias direnv_init="echo 'export PATH=\$PWD/bin:\$PATH' > .envrc && direnv allow"
-alias ggdb='/usr/local/Cellar/gdb/7.7/bin/gdb'
 
 function cd() {builtin cd $@ && ls}
 
