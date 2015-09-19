62d132da9677db2458ee89914b1a76bf0c9ad850
monochromegane <dev.kuro.obi@gmail.com>
2015-09-19 12:23:05 +0900

    cdrの導入とpecoまわりの定義を見直し

--- oh-my-zsh/custom/peco.zsh
+++ oh-my-zsh/custom/peco.zsh
@@ -3,6 +3,16 @@ p() { peco | while read LINE; do $@ $LINE; done }
 
 # ghq list
 alias s='ghq list -p | p cd'
+function peco-ghq () {
+  local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N peco-ghq
+bindkey '^s' peco-ghq
 
 # select history
 function exists { which $1 &> /dev/null }
@@ -16,6 +26,19 @@ function peco-select-history() {
 
 zle -N peco-select-history
 bindkey '^R' peco-select-history
+
+# select change directory history
+function peco-cdr () {
+    local selected_dir=$(cdr -l | awk '{ print $2 }' | peco)
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-cdr
+
+bindkey '^i' peco-cdr
 
 # kill process
 function peco-kill-process() {
--- zshrc
+++ zshrc
@@ -1,3 +1,4 @@
+# oh-my-zsh
 ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="simple"
 
@@ -5,20 +6,28 @@ plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
 
-# User configuration
-
+# Golang
 export GOPATH="$HOME"
 export PATH="$GOPATH/bin:$PATH"
 
+# Ruby
 if  [ -d ~/.rbenv ]; then
   export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
   eval "$(rbenv init -)"
 fi
 
+# direnv
 which direnv > /dev/null 2>&1
 if [ $? -ne 1 ]; then
     eval "$(direnv hook zsh)"
 fi
+
+# cdr
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+zstyle ':chpwd:*' recent-dirs-max 3000
+zstyle ':chpwd:*' recent-dirs-default yes
+zstyle ':completion:*' recent-dirs-insert both
 
 # local settings
 [ -f ~/.zshrc.local ] && source ~/.zshrc.local
